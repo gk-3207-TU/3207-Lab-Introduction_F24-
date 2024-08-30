2e166ef34da1a0af1115fad4dd0ed6b02bacc37d
gk-3207-TU <54708777+gk-3207-TU@users.noreply.github.com>
2024-08-30 11:54:43 -0400

    Update print_random.c
    Incorrect Source for print_random.c

--- print_random.c
+++ print_random.c
@@ -1,9 +1,20 @@
-//Given Project code
-char* rand_string_alloc(size_t size)
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+
+int main()
 {
-     char *s = malloc(size + 1);
-     if (s) {
-         rand_string(s, size);
-     }
-     return s;
+	int a;
+
+	/* seed the randomizer */
+	srand( (unsigned)time(NULL) );
+
+	printf("Today's random word: ");
+	for(a=0;a<7;a++)
+		putchar( randchar() );
+	putchar('\n');
+
+	return(0);
 }
